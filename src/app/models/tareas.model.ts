cf3635f0e67b5990e02a2db8456967ea457c1207
jotaeme <jotaememoreno@gmail.com>
2019-10-22 21:12:55 +0200

    model

---
+++ src/app/models/tareas.model.ts
@@ -0,0 +1,11 @@
+export interface TareasInterface {
+  name: string;
+  isCompleted: boolean;
+}
+
+export class TareasModel implements TareasInterface {
+
+  constructor(
+    public name: string,
+    public isCompleted: boolean) {}
+}
