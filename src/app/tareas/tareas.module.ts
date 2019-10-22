fa19177bbd02fda0e8056a42ad804ec1761f521e
jotaeme <jotaememoreno@gmail.com>
2019-10-22 19:01:06 +0200

    routing anidado

--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { TareasRoutingModule } from './tareas-routing.module';
 import { TareasComponent } from './tareas.component';
 import { MenuComponent } from './menu/menu.component';
+import { TareasSimpleComponent } from './tareas-simple/tareas-simple.component';
+import { TareasPlusModule } from './tareas-plus/tareas-plus.module';
 
 
 @NgModule({
-  declarations: [TareasComponent, MenuComponent],
+  declarations: [TareasComponent, MenuComponent, TareasSimpleComponent],
   imports: [
     CommonModule,
-    TareasRoutingModule
+    TareasRoutingModule,
+    TareasPlusModule
   ]
 })
 export class TareasModule { }
