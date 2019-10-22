b42449b7bb80a3747eb3f26d48421101108aa3a0
jotaeme <jotaememoreno@gmail.com>
2019-10-22 20:10:05 +0200

    fecha + internacionalizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+
+registerLocaleData(localEs, 'es');
 
 @NgModule({
   declarations: [
@@ -14,7 +18,7 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     CoreModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es-ES'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
