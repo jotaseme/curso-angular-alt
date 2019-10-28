47df2462fb1ee780711f4178bbb47351348bdfe3
jotaeme <jotaememoreno@gmail.com>
2019-10-28 18:35:01 +0100

    clase 24/10

--- src/app/share/destacar.directive.spec.ts
+++ src/app/share/destacar.directive.spec.ts
@@ -1,8 +1,3 @@
 import { DestacarDirective } from './destacar.directive';
 
-describe('DestacarDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DestacarDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+
--- src/app/tareas/tareas-simple/tareas-simple.component.ts
+++ src/app/tareas/tareas-simple/tareas-simple.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tareas-simple.component.css']
 })
 export class TareasSimpleComponent implements OnInit {
-
+  texto: string = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio tempora amet et, saepe possimus, in asperiores iusto provident, quibusdam eius obcaecati consequatur facere impedit fugiat culpa voluptatum incidunt. Quaerat, harum.';
   constructor() { }
 
   ngOnInit() {
